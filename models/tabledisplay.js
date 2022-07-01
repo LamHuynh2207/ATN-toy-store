022b431ba6932a1cc17f3ca9c42f1bc1ed5ee808
Lam Huynh <lamhuynh2207@gmail.com>
2022-07-01 14:59:54 +0700

    add delete

--- models/tabledisplay.js
+++ models/tabledisplay.js
@@ -7,13 +7,23 @@ async function getTable(user)
         text: 'SELECT role FROM users WHERE username = $1',
         values: [user]
     };
+    console.log(user)
     var query_data = await pg_conn.query(acc_query);
     var role = query_data.rows[0].role;
-    const table_query = 
-    {
-        text: 'SELECT * FROM products WHERE shop = $1',
-        values: [role]
-    };
+    let table_query = {}
+    if(role == 'director'){
+        table_query=
+        {
+            text: 'SELECT * FROM products'
+        };
+    }
+    else{
+        table_query = 
+        {
+            text: 'SELECT * FROM products WHERE shop = $1',
+            values: [role]
+        };
+    }
     query_data = await pg_conn.query(table_query);
     var dataTable = query_data.rows
     var stringTable = "<table><tr>"
@@ -22,16 +32,26 @@ async function getTable(user)
         var header = "<th>"+headerData[headerIndex]+"</th>"
         stringTable+=header
     }
+    stringTable += "<th> CRUD </th>"; 
     for(let rowIndex in dataTable){
         var bodyTable = "<tr>"
         rowData = dataTable[rowIndex]
+        id_product = rowData[Object.keys(rowData)[0]]
         for(let fieldIndex in rowData){
             var cell = "<td>"+rowData[fieldIndex]+"</td>"
             bodyTable+=cell
         }
+        var get_query = "?id=" + id_product + "&user="+ user 
+        bodyTable += `<td> <a href='/users/edit${get_query}'> Edit/ </a> <a href='/users/delete${get_query}'> Delete </a> </td>`
         bodyTable+="</tr>"
         stringTable+=bodyTable
     }
+    stringTable += "<tr>"
+    for(let headerIndex in headerData){
+        stringTable += "<td> </td>";
+    }
+    stringTable += "<td> <a href='/add'> Add </a> </td>"
+    stringTable += "</tr>"
     stringTable+="</table>"
     console.log(stringTable)
     return stringTable;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
+var authen = require('../models/authenticate');
+var getTable = require('../models/tabledisplay');
+const { query } = require('express');
+const url = require('url')
+const deleteAction = require('../models/DeleteFunction')
 
 router.get('/',(req,res)=>{
     res.send('respond with a resource');
+})
+
+router.post('/', async function(req, res, next) {
+    var auth = await authen(req.body.username, req.body.password);
+    console.log("Check " + auth);
+     if (auth==true) {
+         // Target --> display product table for this user
+         var tableString = await getTable(req.body.username);
+         // console.table(tableString.fields)
+         res.render('users', {
+         title: "User page",
+         message: "Welcome to ATN\n",
+         table: tableString
+        })
+         }
+     else {
+         res.render('login', { message:"Incorrect Username and/or Password!"})
+     }
+ });
+
+ router.get('/delete',async (req,res,next)=>{
+    const queryObject = url.parse(req.url, true).query;
+    var id = parseInt(queryObject['id'])
+    var user = queryObject['user']
+    await deleteAction(id)
+    var tableString = await getTable(user);
+    res.render('users', {
+        title: "User page",
+        message: "Welcome to ATN\n",
+        table: tableString
+       })
 })
 
 module.exports  = router;
